3f4959c8c8b2e90f4c90bc640a639f2adec0f9d1
rachael-ojo <adeolaatilade@gmail.com>
2023-03-16 05:28:31 -0700

    my first low level  programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
- * 
- * return: 0
-*/
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
-{       
-        puts("\""Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-~                                                                
