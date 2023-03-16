24fe87f5b6480ee0482b5637b487edd454ce90f4
rachael-ojo <adeolaatilade@gmail.com>
2023-03-16 06:12:09 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+init main(void)
+{
+	printf("Size of a Char: %i byte(s)\n", sizeof(char));
+	printf("Size of a Char: %i byte(s)\n", sizeof(init));
+	printf("Size of a Char: %i byte(s)\n", sizeof(long init));
+	printf("Size of a Char: %i byte(s)\n", sizeof(long long init));
+	printf("Size of a Char: %i byte(s)\n", sizeof(float));
+	return (0);
+}
