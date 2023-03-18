59889832f288fa667d0996bb37769851786030fc
rachael-ojo <adeolaatilade@gmail.com>
2023-03-17 21:37:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabelts in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar le;
+
+
+	putchar('\n');
+
+	return (0);
+}
