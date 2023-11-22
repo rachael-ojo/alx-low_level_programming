5dd62bb0c82493db7dfc653940045f64dc4e403d
rachael-ojo <adeolaatilade@gmail.com>
2023-11-22 05:55:22 -0800

    Update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list of nodes and set
+ * the head to NULL
+ * @head: pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL && *head == NULL)
+		return;
+
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(*head);
+	*head = NULL;
+}
