68bb6bd2d12fbb99382f99804831562f0c91de96
rachael-ojo <adeolaatilade@gmail.com>
2023-11-28 14:39:21 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
