4666ab216a3c6293afdde45711634321cc59fdb8
rachael-ojo <adeolaatilade@gmail.com>
2023-11-28 13:16:21 -0800

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,10 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buff;
 
-	if (! filename)
+	if (!filename)
 		return (0);
-	
-	fd = open (filename, O_RDONLY);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
@@ -24,9 +24,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 		return (0);
 
-	nrd = read (fd, buff, letters);
+	nrd = read(fd, buff, letters);
 	nwr = write(STDOUT_FILENO, buff, nrd);
+
 	close(fd);
 	free(buff);
+
 	return (nwr);
 }
