85df8894bf130559719298129d4f08974be4f06a
rachael-ojo <adeolaatilade@gmail.com>
2024-01-11 04:24:37 -0800

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,14 @@
  */
 size_t print_dlistint (const dlistint_t *h)
 {
-        size_t count;
+	size_t count;
 
-        if (h == NULL)
-                return (0);
-        for (count = 0; h != NULL; count++)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-        }
-        return (count);
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
 }
