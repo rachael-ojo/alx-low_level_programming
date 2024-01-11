8b3181a66cd12f3d2b0ebbf96f35f58b5ed7126b
rachael-ojo <adeolaatilade@gmail.com>
2024-01-11 04:56:16 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,14 +7,14 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count;
+	size_t length;
 
 	if (h == NULL)
 		return (0);
-	for (count = 0; h != NULL; count++)
+	for (length = 0; h != NULL; length++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (count);
+	return (length);
 }
