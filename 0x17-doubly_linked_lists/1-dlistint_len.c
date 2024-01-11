a3b8349ca31b47afcb31d3dfb9a1f8f8bd353d27
rachael-ojo <adeolaatilade@gmail.com>
2024-01-11 04:44:49 -0800

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print the int data in a double linked list
+ * @h: the head pointer to the list
+ * Return: The number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
