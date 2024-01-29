f8e225917b4e638d59d6084cc97191528bc0ec74
rachael-ojo <adeolaatilade@gmail.com>
2024-01-29 07:33:21 -0800

    Task 2

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# 2-print_alphabet.py
+# Author: Ojo Rachael
+# Description: Print the alphabet in lowercase.
+for letter in range(ord('a'), ord('z') + 1):
+    print(chr(letter), end=" ")
+print()
