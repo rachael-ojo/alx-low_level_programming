7c930e679ff48aa599b364c3326c57ead3cac9e9
rachael-ojo <adeolaatilade@gmail.com>
2024-05-08 14:17:02 -0700

    Updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,42 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    if (array == NULL || size == 0)
+    {
+        printf("Invalid input: array is NULL or size is 0\n");
+        return -1;
+    }
 
-	if (array == NULL)
-		return (-1);
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            printf("Found %d at index: %lu\n", value, i);
+            return i;
+        }
+    }
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
+    printf("%d not found in the array\n", value);
+    return -1;
+}
 
-	return (-1);
+int main(void)
+{
+    int array[] = {10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
+    size_t size = sizeof(array) / sizeof(array[0]);
+    int value1 = 3;
+    int value2 = 42;
+    int value3 = 999;
+
+    printf("Searching for %d:\n", value1);
+    linear_search(array, size, value1);
+
+    printf("\nSearching for %d:\n", value2);
+    linear_search(array, size, value2);
+
+    printf("\nSearching for %d:\n", value3);
+    linear_search(array, size, value3);
+
+    return 0;
 }
